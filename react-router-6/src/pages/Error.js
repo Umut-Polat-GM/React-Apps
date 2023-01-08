ed0ee75b5e6520a52adca70ca20df37ec6b35ee3
Umut-Polat-GM <umut.polat.272@gmail.com>
2023-01-08 16:44:30 +0300

    react-router-6 tutorial use components 03 | React.js

--- react-router-6/src/pages/Error.js
+++ react-router-6/src/pages/Error.js
@@ -1,8 +1,18 @@
+import { Link } from "react-router-dom";
+
 const Error = () => {
   return (
     <section className='section'>
-      <h2>Error</h2>
+      <h2>404</h2>
+      <p>page not found</p>
+      <Link to="/" >back home</Link>
     </section>
   );
 };
 export default Error;
+
+/*
+# Error page 
+diyelim ki kullanıcı adres cubuguna olmayan bir path name gibi bu sayfaya yönledirilecek.
+
+*/
--- react-router-6/src/pages/Home.js
+++ react-router-6/src/pages/Home.js
@@ -1,8 +1,20 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
     <section className='section'>
       <h2>Home Page</h2>
+      <Link to='/about' className="btn">
+        About
+      </Link>
     </section>
   );
 };
 export default Home;
+
+/*
+ # Links
+ <Link to=''> ...</Link> componenti a tag yerine kullanılıyor. ve sayfayı yeniden yüklemeden sayfalar arasına geçiş yapılmasına olanak veriyor.
+ Görüldüğğü gibi "to=''" olarak bir prop var.
+ 
+*/
