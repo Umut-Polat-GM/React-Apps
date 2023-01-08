b8a38b5e1b7c3e4f3da98328d4084a3d2ebbaf8c
Umut-Polat-GM <umut.polat.272@gmail.com>
2023-01-08 16:05:41 +0300

    react-router-6 tutorial First Pages 02 | React.js

--- react-router-6/src/App.js
+++ react-router-6/src/App.js
@@ -1,5 +1,42 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
-  return <h2>React Router 6 Tutorial</h2>;
+  return ( 
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div> Home Page</div>} />
+        <Route path="testing" element={<div> Testing</div>} />
+      </Routes>
+    </BrowserRouter>
+  )
+  
 }
 
 export default App;
+
+
+/*
+# Bazı Notlar React Router 6
+
+react-router-dom  u npm install ile kolaylıkla indirip kullanabiliriz.
+React Router 6  da kullanılarn wraps lar başlıca BrowserRouter, Routes, ve Route dir.
+
+## BrowserRouter
+En dış katmanı sarıp sarmalayacagımız hook. BrowserRouter Browse ile bağlantıyı sağlar
+
+## Routes 
+Routes wrap'ı route taglarınının parrent olarak kullanılıyor.
+
+## Route
+Her sayfa bir tane Route alıyor.
+2 tane props var "path='/'"  ve "element={<div>Home Page</div>}" gibi
+Route wrap'ı path prop u adress cubugunda yazıyor
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div> Home Page</div>} />
+        <Route path="testing" element={<div> Testing</div>} />
+      </Routes>
+    </BrowserRouter>
+
+*/
