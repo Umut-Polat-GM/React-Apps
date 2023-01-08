0e1a61f453201ad096ce3c569f7b1c42bf4e4af0
Umut-Polat-GM <umut.polat.272@gmail.com>
2023-01-08 18:54:54 +0300

    react-router-6 tutorial use index page with nested routes 04 | React.js

--- react-router-6/src/App.js
+++ react-router-6/src/App.js
@@ -3,15 +3,18 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Products from './pages/Products'
 import Error from './pages/Error'
+import SharedLayout from "./pages/SharedLayout"
 
 function App() {
   return ( 
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="products" element={<Products />} />
-        <Route path="*" element={<Error />}/> 
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="products" element={<Products />} />
+          <Route path="*" element={<Error />}/>
+        </Route>
       </Routes>
     </BrowserRouter>
   )
@@ -45,4 +48,9 @@ Route wrap'ı path prop u adress cubugunda yazıyor
       </Routes>
     </BrowserRouter>
 
+*/
+
+/*
+# Nested Routes Nedir
+İç içe geçmiş sayfa demek ve react router da kullanılıyor.
 */
