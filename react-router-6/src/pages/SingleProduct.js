f282fc0792188934a11153511aaf032611058407
Umut-Polat-GM <umut.polat.272@gmail.com>
2023-01-08 22:00:29 +0300

    react-router-6 tutorial use URL Params, NavLink 05 | React.js

--- react-router-6/src/pages/SingleProduct.js
+++ react-router-6/src/pages/SingleProduct.js
@@ -1,9 +1,25 @@
+import { Link, useParams } from 'react-router-dom';
+import products from '../data';
 const SingleProduct = () => {
+  const { productId } = useParams();
+  const product = products.find((product) => product.id === productId);
+  const { image, name } = product;
+
   return (
     <section className='section product'>
-      <h2>single product</h2>
+      <img src={image} alt={name} />
+      <h5>{name}</h5>
+      <Link to='/products'>back to products</Link>
     </section>
   );
 };
 
 export default SingleProduct;
+
+/*
+useParams adress cubugunda productId yi bize dönderiyor.
+useParams consolda okudugumuzda 
+    >Object 
+      productId : "125" (girilen productId sini veriyor)
+      >[[Prototype]]:Object
+*/
